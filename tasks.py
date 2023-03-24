55a7930d9d8b3a3ef2edb36f130daf7870c97750
Viktor Kurti <aplikat@ya.ru>
2023-03-24 09:41:42 +0400

    ci(tasks.py): add invoke tasks
    BREAKING CHANGE:

---
+++ tasks.py
@@ -0,0 +1,22 @@
+"""
+Invoke is a Python (2.7 and 3.4+) library for managing shell-oriented subprocesses
+and organizing executable Python code into CLI-invokable tasks.
+"""
+
+
+from invoke import task
+
+
+@task
+def clean(c):
+    c.run("rm -rf report")
+
+
+@task
+def run_tests(c):
+    c.run("python -m pytest --tb=short")
+
+
+@task
+def create_report(c):
+    c.run("allure serve report/allure_results")
