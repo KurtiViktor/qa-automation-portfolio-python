73fb9d36ae28f88a3efadad027d39472f205ed88
Viktor Kurti <aplikat@ya.ru>
2024-03-23 22:27:56 +0100

    refactor(tests): shut down the frontend tests
    temporary

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,20 @@
-"""
-pytest fixtures offer dramatic improvements
-over the classic xUnit style of setup/teardown functions
-"""
+# """
+# pytest fixtures offer dramatic improvements
+# over the classic xUnit style of setup/teardown functions
+# """
 
-import pytest
+# import pytest
 
-from config import settings
+# from config import settings
 
 
-@pytest.fixture
-def chrome():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--enable-automation")
-    chrome_options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(executable_path=settings.DRIVER_PATH, options=chrome_options)
-    browser.set_driver(driver)
-    browser.timeout = 2
-    yield browser
-    browser.close()
+# @pytest.fixture
+# def chrome():
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.add_argument("--enable-automation")
+#     chrome_options.add_argument("--start-maximized")
+#     driver = webdriver.Chrome(executable_path=settings.DRIVER_PATH, options=chrome_options)
+#     browser.set_driver(driver)
+#     browser.timeout = 2
+#     yield browser
+#     browser.close()
